c77b3e8c19bd75a6f00c738e69a07149b56bb33d
Cornelia Schulz <cornelia.schulz@web.de>
2026-04-02 11:01:34 +0200

    implement writer.sh

---
+++ finder-app/writer.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+writefile=$1
+writestr=$2
+usage="$0 <writefile> <writestr>"
+
+if [ ! $# -eq 2 ]
+then
+    echo "Wrong number of arguments given. Usage: ${usage}."
+    exit 1
+fi
+
+if [ -z ${writefile} ] || [ -z ${writestr} ]
+then
+    echo "At least one parameter was not specified. Usage: ${usage}."
+    exit 1
+fi
+
+mkdir -p $(dirname ${writefile})
+echo "${writestr}" > ${writefile}
+exit 0
